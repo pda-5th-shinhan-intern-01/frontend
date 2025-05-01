8aab9afeede9c0c272f0187049afb648ad448739
kwak <sal091625@gmail.com>
2025-05-01 17:33:10 +0900

    feat(#1): 색상 변수 설정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,13 +6,13 @@ import Footer from "./Footer";
 export default function Layout() {
   return (
     <div className="flex-col min-h-screen w-full">
-      <header className="fixed top-0 w-full z-10 bg-gray-200 h-16">
+      <header className="fixed top-0 w-full z-10 bg-gray-light h-16">
         <Header />
       </header>
       <main className="flex-col flex-grow w-full min-h-screen mt-16">
         <Outlet />
       </main>
-      <footer className="bg-gray-200 h-16">
+      <footer className="bg-gray-light h-16">
         <Footer />
       </footer>
     </div>
