31f544b8eea8195d3fd0d6cc82fe948b560f69b7
kwak <sal091625@gmail.com>
2025-05-01 18:42:11 +0900

    feat(#2): 숫자를 돈 형식으로 변환하는 util 함수 작성

--- src/pages/MainPage/components/StockInfo.jsx
+++ src/pages/MainPage/components/StockInfo.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { formatNumberForMoney } from "../../../utils/formatNumber";
 
 export default function StockInfo({ ticker }) {
   const [stock, setStock] = useState({
     name: "애플",
     price_kr: 303063,
-    price_us: 210.68,
+    price_us: 21068.12345,
     change_amt: 1496,
     change_rate: 0.4,
   });
@@ -28,8 +29,12 @@ export default function StockInfo({ ticker }) {
           <p className="text-gray-md">{ticker}</p>
         </div>
         <div className="flex gap-2 items-center">
-          <p className="text-xl font-semibold">{stock.price_kr}원</p>
-          <p className="text-gray-md text-sm">${stock.price_us}</p>
+          <p className="text-xl font-semibold">
+            {formatNumberForMoney(stock.price_kr)}원
+          </p>
+          <p className="text-gray-md text-sm">
+            ${formatNumberForMoney(stock.price_us)}
+          </p>
           <div
             className={`flex gap-1 items-center text-sm ${
               stock.change_amt > 0
@@ -39,7 +44,7 @@ export default function StockInfo({ ticker }) {
                 : ""
             }`}
           >
-            <p>{stock.change_amt}원</p>
+            <p>{formatNumberForMoney(stock.change_amt)}원</p>
             <p>({stock.change_rate}%)</p>
           </div>
         </div>
