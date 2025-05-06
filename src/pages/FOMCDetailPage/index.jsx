31c24ddb811e79dafedf23def78ad5f2ca6dcab4
goeunP <0young384@gmail.com>
2025-05-06 20:09:08 +0900

    feat(#15): FOMC 디테일 페이지 UI 완료

--- src/pages/FOMCDetailPage/index.jsx
+++ src/pages/FOMCDetailPage/index.jsx
@@ -1,6 +1,73 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Section from "./components/Section";
+import SectionItem from "./components/SectionItem";
+import dummy from "./dummies/dummy.json";
 
-//FOMC 회의록 요약 페이지
+const labels = {
+  laborMarket: "노동 시장",
+  consumptionInvestmentExport: "소비 · 투자 · 수출",
+  inflation: "물가 및 기대 인플레이션",
+  financialMarket: "금융 시장",
+  summary: "요약",
+  qtComment: "QT 관련 언급",
+  dissent: "반대 의견",
+  marketReaction: "시장 반응",
+};
+
 export default function FOMCDetailPage() {
-  return <div>FOMCDetailPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col">
+      {/* 기본 정보 */}
+      <div className="font-bold text-xl flex items-center">
+        <span onClick={() => navigate("/main/fomcs")} className="text-3xl">
+          &lt; &nbsp;
+        </span>
+        {dummy.title}
+      </div>
+      <div>날짜 : {dummy.date}</div>
+      <div className="flex flex-row justify-between">
+        <div className="border-1  px-2 w-fit">
+          금리 {dummy.rateChange} ({dummy.rateRange.from.toFixed(2)}% -&gt;{" "}
+          {dummy.rateRange.to.toFixed(2)}%)
+        </div>
+        <div className="flex flex-row gap-2">
+          <div className="bg-gray-light px-2">영상 보기</div>
+          <div className="bg-gray-light px-2">원본 회의록으로 가기</div>
+        </div>
+      </div>
+      <hr className="my-6" />
+
+      {/* 회의록 요약  */}
+      <Section title="경제 여건 진단">
+        {Object.entries(dummy.economicAssessment).map(([key, value]) => (
+          <SectionItem key={key} title={labels[key] || key} data={value} />
+        ))}
+      </Section>
+
+      <Section title="통화정책 결정">
+        <SectionItem
+          title="정책 금리"
+          data={dummy.policyDecision.policyRateSummary}
+        />
+        <SectionItem
+          title={dummy.policyDecision.qtPolicyTitle}
+          data={dummy.policyDecision.qtPolicyDetails}
+        />
+      </Section>
+
+      <Section title="투표 결과">
+        <SectionItem title="찬성" data={dummy.votes.agreed} />
+        <SectionItem title="반대" data={dummy.votes.disagreed} />
+        <div className="">반대 이유 : {dummy.votes.note} </div>
+      </Section>
+
+      <Section title="추가 언급 및 시장 반응">
+        {Object.entries(dummy.remarks).map(([key, value]) => (
+          <SectionItem key={key} title={labels[key] || key} data={value} />
+        ))}
+      </Section>
+    </div>
+  );
 }
