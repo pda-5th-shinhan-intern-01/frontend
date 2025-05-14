7f54090675f476719bc016b7714ee7c66b885d76
kwak <sal091625@gmail.com>
2025-05-14 11:20:49 +0900

    feat(#30): 검색모달 외부 클릭 시 닫힘 처리

--- src/components/SearchModel.jsx
+++ src/components/SearchModel.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function SearchModel({ results, onClose }) {
   const navigate = useNavigate();
+  const modalRef = useRef(null);
 
   const handleSelectResult = (ticker) => {
     navigate(`/main/sectors/${ticker}`);
     onClose();
   };
 
+  // 바깥 클릭 감지
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        onClose();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
-    <div className="bg-white border mt-2 p-2 flex flex-col gap-2 rounded-md">
+    <div
+      ref={modalRef}
+      className="bg-white border mt-2 p-2 flex flex-col gap-2 rounded-md shadow-md"
+    >
       {results.map((item) => (
         <li
           key={item.ticker}
           className="flex items-center gap-3 hover:bg-gray-light p-2 cursor-pointer"
-          onClick={() => {
-            handleSelectResult(item.ticker);
-          }}
+          onClick={() => handleSelectResult(item.ticker)}
         >
           <img
             src={`${import.meta.env.VITE_STOCK_LOGO_URL}${item.ticker}.png`}
