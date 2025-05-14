e2efcfb9d4853030e0c2b08dbf620c9f58dfccfb
kwak <sal091625@gmail.com>
2025-05-14 10:57:32 +0900

    feat(#30): 검색창 모달 UI 구현

--- src/components/SearchModel.jsx
+++ src/components/SearchModel.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function SearchModel() {
-  return <div>SearchModel</div>;
+export default function SearchModel({ results, onClose }) {
+  const navigate = useNavigate();
+
+  const handleSelectResult = (ticker) => {
+    navigate(`/main/sectors/${ticker}`);
+    onClose();
+  };
+
+  return (
+    <div className="bg-white border mt-2 p-2 flex flex-col gap-2 rounded-md">
+      {results.map((item) => (
+        <li
+          key={item.ticker}
+          className="flex items-center gap-3 hover:bg-gray-light p-2 cursor-pointer"
+          onClick={() => {
+            handleSelectResult(item.ticker);
+          }}
+        >
+          <img
+            src={`${import.meta.env.VITE_STOCK_LOGO_URL}${item.ticker}.png`}
+            className="w-8 h-8 bg-[color:var(--color-gray-light)] rounded-full"
+          />
+          <div className="flex-1">
+            <div className="font-medium text-[color:var(--color-black)]">
+              {item.name}
+            </div>
+            <div className="text-[color:var(--color-gray-md)]">
+              ${item.price.toFixed(2)} ({item.change})
+            </div>
+          </div>
+        </li>
+      ))}
+    </div>
+  );
 }
