81d6875882e1dd4f81e222e4a9c0c85b15f23e2c
kwak <sal091625@gmail.com>
2025-05-16 19:36:08 +0900

    feat(#42): 인트로 UI 디자인 완료

--- src/pages/IntroPage/components/ProblemSection.jsx
+++ src/pages/IntroPage/components/ProblemSection.jsx
@@ -38,55 +38,64 @@ export default function ProblemSection() {
   });
 
   return (
-    <section className="flex flex-col justify-between items-center text-center max-h-[800px] w-full overflow-x-hidden">
-      {/* 상단 문구 */}
-      <div className="mb-40 mt-80">
-        <h2 className="text-5xl md:text-5xl font-semibold">
-          경제 지표 발표, 그냥 흘려보내고 계신가요?
-        </h2>
-        <p className="mt-4 md:text-lg leading-relaxed">
-          CPI, 고용지표, 금리 발표… 이런 뉴스가 주가에 어떤 영향을 주는지 알고
-          계신가요? <br />
-          <span>
-            "시장의 반응을 미리 예측하고, 더 현명한 투자 결정을 내리세요"
-          </span>
-        </p>
-      </div>
+    <section className="w-full">
+      <motion.div
+        className="flex flex-col justify-between items-center text-center max-h-[800px] w-full overflow-x-hidden"
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7, delay: 0.3 }}
+        viewport={{ amount: 0.4 }}
+      >
+        {/* 상단 문구 */}
+        <div className="mb-40 mt-80">
+          <h2 className="text-5xl md:text-5xl font-semibold">
+            경제 지표 발표, 그냥 흘려보내고 계신가요?
+          </h2>
+          <p className="mt-8 md:text-lg leading-relaxed">
+            CPI, 고용지표, 금리 발표… 이런 뉴스가 나올 때마다 놓치고 있지는
+            않나요? <br />
+            <div className="text-red-md font-semibold text-xl mt-4">
+              "경제 지표 이벤트에 대한 시장의 반응을 미리 예측하고, 더 현명한
+              투자 결정을 내리세요"
+            </div>
+          </p>
+        </div>
 
-      <div className="relative w-full h-100 overflow-hidden">
-        <motion.div
-          className="absolute top-[-80px] left-0 w-full h-[150%] z-0"
-          animate={{ y: [0, 24, 0] }}
-          transition={{ duration: 3, repeat: Infinity, ease: "easeInOut" }}
-        >
-          <svg
-            viewBox="0 0 1440 250"
-            className="w-full h-full"
-            preserveAspectRatio="none"
+        <div className="relative w-full h-100 overflow-hidden">
+          <motion.div
+            className="absolute top-[-80px] left-0 w-full h-[150%] z-0"
+            animate={{ y: [0, 24, 0] }}
+            transition={{ duration: 3, repeat: Infinity, ease: "easeInOut" }}
           >
-            <path
-              d="M 0 120 Q 180 60, 360 120 T 720 120 T 1080 120 T 1440 120 V 250 H 0 Z"
-              fill="#DAF4FF"
-            />
-          </svg>
-        </motion.div>
+            <svg
+              viewBox="0 0 1440 250"
+              className="w-full h-full"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M 0 120 Q 180 60, 360 120 T 720 120 T 1080 120 T 1440 120 V 250 H 0 Z"
+                fill="#DAF4FF"
+              />
+            </svg>
+          </motion.div>
 
-        <motion.div
-          className="absolute top-[72%] -translate-y-1/2 whitespace-nowrap text-4xl font-extrabold"
-          animate={{ x: ["-100%", "0%"] }}
-          transition={{
-            repeat: Infinity,
-            duration: 300,
-            ease: "linear",
-          }}
-        >
-          {[...Array(10)].flatMap((_, i) =>
-            animatedWords.map((word, j) => (
-              <span key={`${i}-${j}`}>{word}</span>
-            ))
-          )}
-        </motion.div>
-      </div>
+          <motion.div
+            className="absolute top-[72%] -translate-y-1/2 whitespace-nowrap text-4xl font-extrabold"
+            animate={{ x: ["-100%", "0%"] }}
+            transition={{
+              repeat: Infinity,
+              duration: 300,
+              ease: "linear",
+            }}
+          >
+            {[...Array(10)].flatMap((_, i) =>
+              animatedWords.map((word, j) => (
+                <span key={`${i}-${j}`}>{word}</span>
+              ))
+            )}
+          </motion.div>
+        </div>
+      </motion.div>
     </section>
   );
 }
