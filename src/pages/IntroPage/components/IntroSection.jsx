046cb43b1911885a286687dd94d9298ad167060c
kwak <sal091625@gmail.com>
2025-05-16 09:59:36 +0900

    feat(#39): 초록 색상 변수 추가

--- src/pages/IntroPage/components/IntroSection.jsx
+++ src/pages/IntroPage/components/IntroSection.jsx
@@ -8,16 +8,19 @@ export default function IntroSection() {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
       viewport={{ amount: 0.5 }}
-      className="bg-black text-white min-h-screen flex flex-col justify-center items-center text-center px-4"
+      className="items-center flex flex-col justify-center bg-gradient-to-t from-red-md to-orange text-white min-h-screen "
     >
-      <img src={logo} className="w-140"></img>
-      <h2 className="mt-4 text-lg md:text-3xl font-bold">
-        지표를 통해 종목의 Hot Signal을 확인하세요!
-      </h2>
-      <p className="mt-5 text-lg">
-        세계 경제 캘린더와 종목별 민감도 분석으로
-        <br />한 발 앞선 투자 인사이트를 제공합니다
-      </p>
+      <div className="flex flex-col justify-center max-w-[1200px] w-full">
+        {/* <img src={logo} className="w-140"></img> */}
+        <h1 className="text-9xl font-semibold mb-8">HotSignal</h1>
+        <h2 className="mt-4 text-lg md:text-3xl font-bold">
+          지표를 통해 종목의 Hot Signal을 확인하세요
+        </h2>
+        <p className="mt-5 text-lg">
+          세계 경제 캘린더와 종목별 민감도 분석으로
+          <br />한 발 앞선 투자 인사이트를 제공합니다
+        </p>
+      </div>
     </motion.section>
   );
 }
--- src/pages/IntroPage/components/ProblemSection.jsx
+++ src/pages/IntroPage/components/ProblemSection.jsx
@@ -1,24 +1,92 @@
 import { motion } from "framer-motion";
 
 export default function ProblemSection() {
+  const indicators = [
+    "CPI",
+    "PPI",
+    "Core PCE",
+    "NFP",
+    "Unemployment Rate",
+    "Retail Sales",
+    "ISM",
+    "Real GDP YoY",
+  ];
+
+  const animatedWords = indicators.map((text, i) => {
+    const yRange = Math.floor(Math.random() * 20) + 5;
+    const rotateRange = Math.floor(Math.random() * 15) + 2;
+    const delay = Math.random() * 2.5;
+
+    return (
+      <motion.span
+        key={i}
+        className="inline-block mx-36"
+        animate={{
+          y: [0, -yRange, 0, yRange, 0],
+          rotate: [-rotateRange, 0, rotateRange, 0, -rotateRange],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 15 + Math.random(),
+          delay,
+          ease: "easeInOut",
+        }}
+      >
+        {text}
+      </motion.span>
+    );
+  });
   return (
     <motion.section
       initial={{ opacity: 0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
       viewport={{ amount: 0.4 }}
-      className="min-h-screen flex flex-col justify-center items-center text-center px-6 gap-1"
+      className="flex flex-col justify-center items-center text-center min-h-screen"
     >
-      <h2 className="text-2xl md:text-4xl font-semibold">
-        경제 지표 발표, 그냥 흘려보내고 계신가요?
-      </h2>
-      <p className="mt-4 md:text-lg leading-relaxed">
-        CPI, 고용지표, 금리 발표… 이런 뉴스가 주가에 어떤 영향을 주는지 알고
-        계신가요? <br />
-        <span>
-          "시장의 반응을 미리 예측하고, 더 현명한 투자 결정을 내리세요"
-        </span>
-      </p>
+      <div className="mb-40">
+        <h2 className="text-5xl md:text-4xl font-semibold">
+          경제 지표 발표, 그냥 흘려보내고 계신가요?
+        </h2>
+        <p className="mt-4 md:text-lg leading-relaxed">
+          CPI, 고용지표, 금리 발표… 이런 뉴스가 주가에 어떤 영향을 주는지 알고
+          계신가요? <br />
+          <span>
+            "시장의 반응을 미리 예측하고, 더 현명한 투자 결정을 내리세요"
+          </span>
+        </p>
+      </div>
+
+      {/* <div className="relative w-full h-24 mb-12">
+        <svg
+          viewBox="0 0 1440 150"
+          className="absolute top-0 left-0 w-full h-full pointer-events-none"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M 0 100 Q 240 0, 480 100 T 960 100 T 1440 100"
+            fill="transparent"
+            stroke="#333"
+            strokeWidth="1"
+          />
+        </svg>
+
+        <motion.div
+          className="absolute top-1/2 -translate-y-1/2 whitespace-nowrap text-4xl font-extrabold"
+          animate={{ x: ["-100%", "0%"] }}
+          transition={{
+            repeat: Infinity,
+            duration: 100,
+            ease: "linear",
+          }}
+        >
+          {[...Array(10)].flatMap((_, i) =>
+            animatedWords.map((word, j) => (
+              <span key={`${i}-${j}`}>{word}</span>
+            ))
+          )}
+        </motion.div>
+      </div> */}
     </motion.section>
   );
 }
