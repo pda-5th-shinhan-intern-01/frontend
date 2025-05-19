bcc5eb1b0ebad4681d678eaef78d6856ced305aa
kwak <sal091625@gmail.com>
2025-05-19 13:08:27 +0900

    feat(#57): fomc 디테일 디자인 수정

--- src/pages/FOMCDetailPage/components/CommonSection.jsx
+++ src/pages/FOMCDetailPage/components/CommonSection.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function CommonSection({ title, data, label }) {
   return (
     <div className="flex flex-col">
-      <h2 className="font-semibold mb-3 text-xl">{title}</h2>
+      <h2 className="font-semibold mb-3 text-2xl">{title}</h2>
       {/* 키워드 */}
       {data.keywords && (
         <div className="flex flex-col gap-1 mb-3">
           <div className="flex flex-row gap-3">
             {data.keywords.map((word) => (
-              <div className="rounded-md  text-sm py-1 px-2 bg-blue-light text-white">
+              <div className="rounded-full  text-sm py-1 px-4 bg-orange text-white">
                 {word}
               </div>
             ))}
@@ -17,13 +17,13 @@ export default function CommonSection({ title, data, label }) {
         </div>
       )}
 
-      <div className="p-4 rounded-lg bg-white border border-gray-light flex flex-col gap-3">
+      <div className="p-8 rounded-2xl bg-white border shadow-md border-gray-light flex flex-col gap-3">
         {Object.entries(data).map(([key, value]) => {
           return (
             value &&
             key !== "keywords" && (
               <div key={key} className="flex flex-col gap-1">
-                <div className="text-gray-md font-semibold">- {label[key]}</div>
+                <div className="font-semibold">- {label[key]}</div>
                 <div className="pl-3">{value}</div>
               </div>
             )
